b9e4dec46a3901cd253b58107cf4ce5818027d99
Sofia Torres <128488303+sotorres01@users.noreply.github.com>
2024-05-20 01:59:34 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Auth0Provider
+    domain="http://dev-82racnsjh577pk1m.us.auth0.com/"
+    clientId="zQhRYvyBVEL8FH30GAxpnH2nKmlPGbBQ"
+    client_secret="IgJYRnFsWanLvMQP0Rq73tbhrtN4R_-jLPPfzNv58se80BlOmA7UIUvRCkwbSRbr"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
